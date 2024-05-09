705951dd52a0754dafb600f9a0695b07d3ef0446
Robert <poprobert1999@yahoo.com>
2024-05-09 20:08:25 +0300

    Did the calculator function. The project it's at 50%

--- main.py
+++ main.py
@@ -12,5 +12,21 @@ def substract(a, b):
 
 
 operation = {
-    '*':multiply
+    '+': add,
+    '-': substract,
+    '*': multiply,
+    '/': divide
 }
+
+def calculator():
+    print(paint)
+    num1 = int(input("What is the first number?: "))
+    for n in operation:
+        print(n)
+    symbols = input("Pick an operation symbol from the line above: ")
+    num2 = int(input("What is the second number?: "))
+    function = operation[symbols]
+    answer = function(num1, num2)
+    print(f"{num1} {symbols} {num2} = {answer}")
+
+calculator()
